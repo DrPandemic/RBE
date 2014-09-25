ac6f5a98259169d8b19014c01127ed89bce380dc
Jesse Emond <emond.jesse@gmail.com>
2014-09-24 22:05:09 -0400

    + rbe.py file for exploiting the vulnerability

---
+++ rbe.py
@@ -0,0 +1,13 @@
+# Remote Bash Execution
+
+# rbe.py
+# Exploiting the shellshock vulnerability ( see https://github.com/Parasithe/RBE )
+
+# /JesseEmond
+# /Parasithe
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Remote Base Execution - proof of concept of the shellshock vulnerability')
+
+args = parser.parse_args()
